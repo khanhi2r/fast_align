a74a26dd2fcb09e05aef5fa2e6d5f2f6445cd7b8
Nguyen Ngoc Khanh <nguyen_ngoc_khanh@i2r.a-star.edu.sg>
2024-06-04 14:55:01 +0800

    auto_push

--- make_parallel_data.py
+++ make_parallel_data.py
@@ -5,11 +5,11 @@ from tqdm import tqdm
 
 
 bm_text_path_list = [
-    "/home/khanh/ws/rw_10_2_56_217/scripts/db/english_to_malay_text_mesolitica",
+    "/home/khanh/ws/rw_10_2_56_142/scripts/db/english_to_malay_text_mesolitica_malayprompt",
 ]
 
 en_text_path_list = [
-    "/home/khanh/ws/rw_10_2_56_217/scripts/db/english_text"
+    "/home/khanh/ws/rw_10_2_56_142/scripts/db/english_text"
 ]
 
 data_out_path = "data/data.en-bm"
@@ -20,21 +20,25 @@ with open(data_out_path, "w") as f:
 
     for bm_text_path in bm_text_path_list:
         for utt, text in tqdm(list(read_table(open(bm_text_path), ts=(str, str))), desc=f"loading {bm_text_path} ..."):
-            text = clean_line(text)
+
+            text = clean_line(text).lower()
+
             if len(text) == 0:
                 continue
             bm_dict[utt] = text
 
     for en_text_path in en_text_path_list:
         for utt, text in tqdm(list(read_table(open(en_text_path), ts=(str, str))), desc=f"loading {en_text_path} ..."):
-            en_text = clean_line(text)
-            if len(en_text) == 0:
+            
+            text = clean_line(text).lower()
+
+            if len(text) == 0:
                 continue
             if utt not in bm_dict:
                 continue
             bm_text = bm_dict[utt]
 
-            f.write(f"{en_text} ||| {bm_text}\n")
+            f.write(f"{text} ||| {bm_text}\n")
 
 
             
